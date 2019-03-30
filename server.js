19daf790b78d056741ac8cebb7e5ad5b2a99466c
Francisco <hidalgo_fco520@outlook.com>
2019-03-30 04:42:54 -0600

    prepare to heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 if(process.env.NODE_ENV != 'test') {
-    mongoose.connect('mongodb://localhost:27017/dna', { useNewUrlParser: true })
+    mongoose.connect('mongodb://heroku_5hc4vf46:27re389lc885b1rii5cgukhdl@ds147003.mlab.com:47003/heroku_5hc4vf46', { useNewUrlParser: true })
     .then(req => {
         console.log('conexion a la base de datos echa');
         server.listen(server.get('port'), () => console.log('App corriendo en el puerto: ', server.get('port')));
