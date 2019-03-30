4cfeb2f9150696833b6d5b0f249c942cb2f1fd96
Francisco <hidalgo_fco520@outlook.com>
2019-03-30 12:17:34 -0600

    REady for GutHub

--- levelthree/routes/statsrouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const StatsController = require('../controllers/StatsController');
-const router = express.Router();
-
-router.get('/stats', StatsController.show);
-
-module.exports = router;
